c870f814a53bb95264fef47a2e822b985e361727
JaviZamoraDev <javier.zamora.dev@gmail.com>
2024-10-27 22:24:40 -0300

    [ADD] - New module in version 13.0

---
+++ account_move_extra/__manifest__.py
@@ -0,0 +1,11 @@
+
+{
+    "name": "Account Move Extra",
+    "version": "13.0.1.0.0",
+    "author": "Javier Zamora",
+    "website": "https://github.com/JaviZamoraDev/odoo-apps",
+    "license": "AGPL-3",
+    "category": "Accounting",
+    "depends": ["account"],
+    "data": [],
+}
